115d4b9905c2c400aaad37c6aff476e190d6842a
777Antoniy777 <tat7106@yandex.ru>
2021-01-25 19:36:15 +0600

    add basic types, array and turples

---
+++ types/types.js
@@ -0,0 +1,10 @@
+// simple types
+var str = 'name';
+var isLoading = false;
+var num = 42;
+console.log(str);
+// arrays
+var arr = [1, 2, 3, 4];
+var arr2 = [1, 2, 3];
+// turple
+var turple = ['word', false, 123];
---
+++ types/types.ts
@@ -0,0 +1,13 @@
+// simple types
+const str: string = 'name';
+const isLoading: boolean = false;
+let num: number = 42;
+
+console.log(str);
+
+// arrays
+const arr: number[] = [1, 2, 3, 4];
+const arr2: Array<number> = [1, 2, 3];
+
+// turple
+const turple: [string, boolean, number] = ['word', false, 123];
